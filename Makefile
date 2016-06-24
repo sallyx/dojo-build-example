ae4500a61725a53b45b2172162903f6fac4307b0
sallyx <sallyx@centrum.cz>
2016-06-24 08:44:47 +0200

    ratchet hello world

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 build:
 	rm -rf ./release
 	./src/util/buildscripts/build.sh --profile my  --localeList "en-gb,en-us,cs-cz"
+install:
+	composer require cboden/ratchet
---
+++ bin/chat-server.php
@@ -0,0 +1,13 @@
+<?php
+require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/MyApp/Chat.php';
+
+use Ratchet\Server\IoServer;
+use MyApp\Chat;
+
+$server = IoServer::factory(
+        new Chat(),
+        8080
+);
+
+$server->run();
